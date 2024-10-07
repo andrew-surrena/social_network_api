2378cdc906fa7057ef551de80de363a353722d7a
andrew-surrena <andrewsurrena@gmail.com>
2024-10-07 17:14:03 -0500

    added server & connection routes

---
+++ src/routes/api/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+const router = Router();
+import thoughtRoutes from './thoughtRoutes.js';
+import userRoutes from './userRoutes.js';
+import reactionRoutes from './reactionRoutes.ts';
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+router.use('/reactions', reactionRoutes);
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import db from './config/connection.js';
+import routes from './routes/index.js';
+
+const cwd = process.cwd();
+
+const PORT = 3001;
+const app = express();
+
+// Note: not necessary for the Express server to function. This just helps indicate what project's server is running in the terminal.
+const project = cwd.includes('social_network_api')
+  ? cwd.split('social_network_api')[0]
+  : cwd;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${project} running on port ${PORT}!`);
+  });
+});
